311d2e84e6f64dd6ca7430bf1ddf9b4ac26b967e
Anel CC <anel.calvo.cervantes@gmail.com>
2020-07-13 19:00:12 -0500

    [RxJava003_SimpleNetworking] 10.- Simple networking

--- app/src/main/java/com/anelcc/simplerxjava/activities/BasicExampleActivity.kt
+++ app/src/main/java/com/anelcc/simplerxjava/activities/BasicExampleActivity.kt
@@ -3,10 +3,37 @@ package com.anelcc.simplerxjava.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.anelcc.simplerxjava.R
+import com.anelcc.simplerxjava.modellayer.entities.Posting
+import io.reactivex.disposables.CompositeDisposable
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 class BasicExampleActivity : AppCompatActivity() {
 
+    private var bag = CompositeDisposable()
+
+    //region Simple Network Layer
+
+    interface JsonPlaceHolderService {
+        //So whatever value we pass in as id will fill in that field on the service.
+        // And this is gonna return a call and it's gonna get a posting.
+        @GET("posts/{id}")
+        fun getPosts(@Path("id") id: String): Call<Posting>
+    }
+
+    //Create an instance of Retrofit.
+    private var retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl("https://jsonplaceholder.typicode.com/")
+        .build()
+
+    private var service = retrofit.create(JsonPlaceHolderService::class.java)
+
+    //endregion
 
     //region Life Cycle Events
 
