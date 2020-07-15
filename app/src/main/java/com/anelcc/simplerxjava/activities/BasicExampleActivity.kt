8da6c376e2694bcdc68e38bac48f297a59ecef43
Anel CC <anel.calvo.cervantes@gmail.com>
2020-07-14 19:51:03 -0500

    [RxJava003_SimpleNetworking] 11.-  consume  the network layer

--- app/src/main/java/com/anelcc/simplerxjava/activities/BasicExampleActivity.kt
+++ app/src/main/java/com/anelcc/simplerxjava/activities/BasicExampleActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.anelcc.simplerxjava.R
 import com.anelcc.simplerxjava.modellayer.entities.Posting
+import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.io.IOException
 
 
 class BasicExampleActivity : AppCompatActivity() {
@@ -40,7 +44,38 @@ class BasicExampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basic_example)
+
+        realSingleExample()
     }
 
+    //region Rx Code
+    private fun realSingleExample() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun loadPostAsSingle(): Single<Posting> {
+        return Single.create { observe ->
+            //simulate network
+            Thread.sleep(2000)
+            val postingId = 5
+            service.getPosts(postingId.toString()).enqueue(object : Callback<Posting>{
+                override fun onFailure(call: Call<Posting>, t: Throwable) {
+                    val e = t ?: IOException("An Unknown network error occurred")
+                    observe.onError(e)
+                }
+
+                override fun onResponse(call: Call<Posting>, response: Response<Posting>) {
+                    val posting = response?.body()
+                    if (posting != null) {
+                        observe.onSuccess(posting)
+                    } else {
+                        val e = IOException("An Unknown network error occurred")
+                        observe.onError(e)
+                    }
+                }
+
+            })
+        }
+    }
     //endregion
 }
