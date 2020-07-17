191e84d88b9b7d05f11fd1d02c62f3ba3ba58056
Anel CC <anel.calvo.cervantes@gmail.com>
2020-07-16 23:43:09 -0500

    [RxJava003_SimpleNetworking] 14.-  Listening to our BehaviorRelay and notified that new values.

--- app/src/main/java/com/anelcc/simplerxjava/modellayer/ModelLayer.kt
+++ app/src/main/java/com/anelcc/simplerxjava/modellayer/ModelLayer.kt
@@ -2,6 +2,8 @@ package com.anelcc.simplerxjava.modellayer
 
 import com.anelcc.simplerxjava.modellayer.networklayer.NetworkLayer
 import com.anelcc.simplerxjava.modellayer.persistencelayer.PersistenceLayer
+import com.anelcc.simplerxjava.modellayer.persistencelayer.PhotoDescription
+import com.jakewharton.rxrelay2.BehaviorRelay
 
 class ModelLayer {
 
@@ -9,6 +11,18 @@ class ModelLayer {
         val shared = ModelLayer()
     }
 
+    //As we get data, that'll change and we can react to that, because it's a BehaviorRelay.
+    val photoDescriptions = BehaviorRelay.createDefault(listOf<PhotoDescription>())
+
     private val networkLayer = NetworkLayer.instance
     private val persistenceLayer = PersistenceLayer.shared
+
+    //Loading data from the database often can be a longer process,
+    // so it is something that you would normally want to put on a background thread anyway.
+    fun loadAllPhotoDescriptions() { //result may be immediate, but use async callbacks
+        persistenceLayer.loadAllPhotoDescriptions { photoDescriptions ->
+            // Anyone who's listening to our photoDescriptions BehaviorRelay, they'll be notified that new values.
+            this.photoDescriptions.accept(photoDescriptions)
+        }
+    }
 }
