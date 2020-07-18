080f815b60453c5c5a3130c288e2afd931c4f703
Anel CC <anel.calvo.cervantes@gmail.com>
2020-07-18 11:44:19 -0500

    [RxJava003_SimpleNetworking] 16.-  Database: Showing our result, Consume presenter this in the activity or really to anyone who's observing on it.

--- app/src/main/java/com/anelcc/simplerxjava/activities/databaseexample/DatabaseExampleActivity.kt
+++ app/src/main/java/com/anelcc/simplerxjava/activities/databaseexample/DatabaseExampleActivity.kt
@@ -6,12 +6,16 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.anelcc.simplerxjava.R
 import com.anelcc.simplerxjava.activities.databaseexample.recycler.PhotoDescriptionViewAdapter
+import com.anelcc.simplerxjava.common.disposedBy
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_database_example.*
 
-
+// Consume presenter this in the activity or really to anyone who's observing on it.
 class DatabaseExampleActivity : AppCompatActivity() {
 
     private val presenter = DatabaseExamplePresenter()
+    private var bag = CompositeDisposable()
 
     lateinit var adapter: PhotoDescriptionViewAdapter
 
@@ -19,8 +23,17 @@ class DatabaseExampleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_database_example)
 
-        attachUI()
-    }
+        // We'll consume to the onCreate method.
+        presenter.photoDescriptions
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { descriptions ->
+                //just print the descriptions
+                descriptions.forEach { println("🕺: $it") }
+                //When we get new values from the database we should set the adapter  and accepted this values toMutableList
+                adapter.photoDescriptions.accept(descriptions.toMutableList())
+            }.disposedBy(bag)
+
+        attachUI()    }
 
     private fun attachUI() {
         val linearLayoutManager = LinearLayoutManager(this)
@@ -41,6 +54,14 @@ class DatabaseExampleActivity : AppCompatActivity() {
 
     private fun rowTapped(position: Int) {
         println("🍄")
-        println(adapter.photoDescriptions[position])
+        //We've also made changes to that photoDescriptions and made it a behavior relay.
+        println(adapter.photoDescriptions.value[position])
+    }
+
+    // Inside on the onDestroy let's clear out that bag, we'll say bag.clear.
+    // That'll remove all subscriptions when the onDestroy event comes through.
+    override fun onDestroy() {
+        super.onDestroy()
+        bag.clear()
     }
 }
