5381f3c1582b61a7da531b6714a18f1ac3947e6c
Anel CC <anel.calvo.cervantes@gmail.com>
2020-07-20 21:31:49 -0500

    [RxJava003_SimpleNetworking] 17.- Network: From model layer to UI Moving the data that comes from these network calls up through the different layers and up to the UI. 

--- app/src/main/java/com/anelcc/simplerxjava/activities/networkexample/NetworkExamplePresenter.kt
+++ app/src/main/java/com/anelcc/simplerxjava/activities/networkexample/NetworkExamplePresenter.kt
@@ -1,7 +1,19 @@
 package com.anelcc.simplerxjava.activities.networkexample
 
 import com.anelcc.simplerxjava.modellayer.ModelLayer
+import com.anelcc.simplerxjava.modellayer.entities.Message
+import com.jakewharton.rxrelay2.BehaviorRelay
+import io.reactivex.disposables.CompositeDisposable
 
 class NetworkExamplePresenter {
     private val modelLayer = ModelLayer.shared //normally injected
+
+    val message: BehaviorRelay<List<Message>>
+    get() = modelLayer.messages
+
+    private var bag = CompositeDisposable()
+
+    init {
+        modelLayer.getMessages()
+    }
 }
