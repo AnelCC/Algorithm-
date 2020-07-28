cbd59a48ecefdf1b5f019b9dfb6396d3cb36fe55
Anel CC <anel.calvo.cervantes@gmail.com>
2020-07-28 10:42:38 -0500

    [RxJava006_BindingUI] 29.- Simple binding

--- app/src/main/java/com/anelcc/simplerxjava/activities/reactiveui/simple/SimpleUIActivity.kt
+++ app/src/main/java/com/anelcc/simplerxjava/activities/reactiveui/simple/SimpleUIActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.anelcc.simplerxjava.R
+import com.anelcc.simplerxjava.common.disposedBy
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_simple_ui.*
 
 class SimpleUIActivity : AppCompatActivity() {
 
     private val presenter = SimpleUIPresenter()
     private lateinit var adapter: ArrayAdapter<String>
+    private var bag = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +23,25 @@ class SimpleUIActivity : AppCompatActivity() {
     }
 
     private fun rxExamples() {
+        simpleRx()
         rxSimpleListBind()
+    }
+
+    private fun simpleRx() {
+        /*presenter.title.observeOn(AndroidSchedulers.mainThread())
+                .subscribe { simpleUITitleTextView.text = it }
+                .disposedBy(bag)*/
+
+        presenter.title.observeOn(AndroidSchedulers.mainThread())
+            .onErrorReturn { "Default Value" } // this error is recieve Faked Error but we ignore and setting Default Value
+            .share()
+            .subscribe { simpleUITitleTextView.text = it }
+            .disposedBy(bag)
     }
 
 
     private fun rxSimpleListBind() {
-        val listItems = presenter.friends.map { it.toString() }
+        val listItems = presenter.friends.value.map { it.toString() }
 
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
 
