a8b1a10bbcbcae4a787265c3e6c78c76b8bc1b47
Anel CC <anel.calvo.cervantes@gmail.com>
2020-07-28 10:11:45 -0500

    [RxJava006_BindingUI] 28.- Prepare reactive presenter

--- app/src/main/java/com/anelcc/simplerxjava/activities/reactiveui/simple/SimpleUIPresenter.kt
+++ app/src/main/java/com/anelcc/simplerxjava/activities/reactiveui/simple/SimpleUIPresenter.kt
@@ -1,27 +1,57 @@
 package com.anelcc.simplerxjava.activities.reactiveui.simple
 
 import com.anelcc.simplerxjava.modellayer.entities.Friend
+import com.jakewharton.rxrelay2.BehaviorRelay
+import io.reactivex.subjects.BehaviorSubject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SimpleUIPresenter {
-    var friends = listOf(
-        Friend("Debi", "Darlington"),
-                         Friend("Arlie", "Abalos"),
-                         Friend("Jessica", "Jetton"),
-                         Friend("Tonia", "Threlkeld"),
-                         Friend("Donte", "Derosa"),
-                         Friend("Nohemi", "Notter"),
-                         Friend("Rod", "Rye"),
-                         Friend("Simonne", "Sala"),
-                         Friend("Kathaleen", "Kyles"),
-                         Friend("Loan", "Lawrie"),
-                         Friend("Elden", "Ellen"),
-                         Friend("Felecia", "Fortin"),
-                         Friend("Fiona", "Fiorini"),
-                         Friend("Joette", "July"),
-                         Friend("Beverley", "Bob"),
-                         Friend("Artie", "Aquino"),
-                         Friend("Yan", "Ybarbo"),
-                         Friend("Armando", "Araiza"),
-                         Friend("Dolly", "Delapaz"),
-                         Friend("Juliane", "Jobin"))
+    val friends = BehaviorRelay.createDefault(listOf<Friend>())
+    val title = BehaviorSubject.createDefault("Default Title")
+
+    init {
+        title.onNext("Load Friends")
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(3000)
+            title.onNext("Friends Loaded")
+
+            var newFriends = listOf(
+                Friend("Debi", "Darlington"),
+                Friend("Arlie", "Abalos"),
+                Friend("Jessica", "Jetton"),
+                Friend("Tonia", "Threlkeld"),
+                Friend("Donte", "Derosa"),
+                Friend("Nohemi", "Notter"),
+                Friend("Rod", "Rye"),
+                Friend("Simonne", "Sala"),
+                Friend("Kathaleen", "Kyles"),
+                Friend("Loan", "Lawrie"),
+                Friend("Elden", "Ellen"),
+                Friend("Felecia", "Fortin"),
+                Friend("Fiona", "Fiorini"),
+                Friend("Joette", "July"),
+                Friend("Beverley", "Bob"),
+                Friend("Artie", "Aquino"),
+                Friend("Yan", "Ybarbo"),
+                Friend("Armando", "Araiza"),
+                Friend("Dolly", "Delapaz"),
+                Friend("Juliane", "Jobin"))
+            CoroutineScope(Dispatchers.IO).launch {
+                friends.accept(newFriends)
+            }
+        }
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(6000) //6 seconds
+            title.onError(Exception("Faked Error"))
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(7000) //7 seconds
+            println("🦄 pushing new value")
+            title.onNext("new Value")
+        }
+    }
 }
