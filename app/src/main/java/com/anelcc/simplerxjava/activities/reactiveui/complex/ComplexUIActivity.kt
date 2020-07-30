559812cf7e39f948ce73287616feff4430337626
Anel CC <anel.calvo.cervantes@gmail.com>
2020-07-30 10:13:58 -0500

    [RxJava006_BindingUI] 32.- RecyclerView: Binding setup

--- app/src/main/java/com/anelcc/simplerxjava/activities/reactiveui/complex/ComplexUIActivity.kt
+++ app/src/main/java/com/anelcc/simplerxjava/activities/reactiveui/complex/ComplexUIActivity.kt
@@ -3,6 +3,8 @@ package com.anelcc.simplerxjava.activities.reactiveui.complex
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.anelcc.simplerxjava.R
+import io.reactivex.disposables.CompositeDisposable
+import com.anelcc.simplerxjava.databinding.ActivityComplexUiBinding
 
 private enum class CellType {
     ITEM,
@@ -12,9 +14,22 @@ private enum class CellType {
 class ReactiveUIActivity : AppCompatActivity() {
 
     private val dataSet = (0..100).toList().map { it.toString() }
+    private var bag = CompositeDisposable()
+    lateinit var boundActivity: ActivityComplexUiBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_complex_ui)
+
+        commonInit()
+        showSimpleBindingExample()
+    }
+
+    private fun showSimpleBindingExample() {
+
+    }
+
+    private fun commonInit() {
+
     }
 }
