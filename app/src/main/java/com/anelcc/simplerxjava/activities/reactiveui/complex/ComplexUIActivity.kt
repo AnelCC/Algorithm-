931917a0d800d19578b19beb352a4926a4bc3e97
Anel CC <anel.calvo.cervantes@gmail.com>
2020-07-30 10:37:21 -0500

    [RxJava006_BindingUI] 33.-RecyclerView: Seeing it in action

--- app/src/main/java/com/anelcc/simplerxjava/activities/reactiveui/complex/ComplexUIActivity.kt
+++ app/src/main/java/com/anelcc/simplerxjava/activities/reactiveui/complex/ComplexUIActivity.kt
@@ -2,9 +2,14 @@ package com.anelcc.simplerxjava.activities.reactiveui.complex
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.anelcc.simplerxjava.R
+import com.anelcc.simplerxjava.common.disposedBy
 import io.reactivex.disposables.CompositeDisposable
 import com.anelcc.simplerxjava.databinding.ActivityComplexUiBinding
+import com.anelcc.simplerxjava.databinding.ItemReactiveUiBinding
+import com.minimize.android.rxrecycleradapter.RxDataSource
 
 private enum class CellType {
     ITEM,
@@ -12,7 +17,6 @@ private enum class CellType {
 }
 
 class ReactiveUIActivity : AppCompatActivity() {
-
     private val dataSet = (0..100).toList().map { it.toString() }
     private var bag = CompositeDisposable()
     lateinit var boundActivity: ActivityComplexUiBinding
@@ -25,11 +29,27 @@ class ReactiveUIActivity : AppCompatActivity() {
         showSimpleBindingExample()
     }
 
-    private fun showSimpleBindingExample() {
-
+    private fun commonInit() {
+        boundActivity = DataBindingUtil.setContentView(this, R.layout.activity_complex_ui)
+        boundActivity.reactiveUIRecyclerView.layoutManager = LinearLayoutManager(this)
     }
 
-    private fun commonInit() {
+    private fun showSimpleBindingExample() {
+        val rxDataSource = RxDataSource<ItemReactiveUiBinding, String>(R.layout.item_reactive_ui, dataSet)
+        rxDataSource.bindRecyclerView(boundActivity.reactiveUIRecyclerView)
 
+        rxDataSource.map { it.toUpperCase() }
+            .asObservable()
+            .subscribe {
+                val ui = it.viewDataBinding ?: return@subscribe
+                val data = it.item
+
+                ui.textViewItem.text = data
+            }.disposedBy(bag)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        bag.clear()
     }
 }
